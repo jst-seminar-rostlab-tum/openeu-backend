24852d29686cf44032f0b824730ad83b5176f541
Pascal Moser <pascal.moser@tum.de>
2025-05-11 13:24:53 +0200

    add own router for the crawler

---
+++ app/dependencies.py
@@ -0,0 +1,13 @@
+import os
+from typing import Optional
+
+from fastapi import Header, HTTPException
+
+CRAWLER_API_KEY = os.getenv("CRAWLER_API_KEY")
+
+if not CRAWLER_API_KEY:
+    raise RuntimeError("CRAWLER_API_KEY environment variable is not set")
+
+async def get_token_header(token: Optional[str] = Header(None)):
+    if token != CRAWLER_API_KEY:
+        raise HTTPException(status_code=403)
--- main.py
+++ main.py
@@ -1,38 +1,9 @@
-import os
-from datetime import datetime
-from threading import Lock
-from typing import Optional
+from fastapi import FastAPI
+from app.api import crawler
 
-from fastapi import FastAPI, HTTPException, Header
-
-# Track application start time
-start_time = datetime.utcnow()
-
-# Counter and lock for thread safety
-call_count = 0
-lock = Lock()
-
-CRAWLER_API_KEY = os.getenv("CRAWLER_API_KEY")
-
-if not CRAWLER_API_KEY:
-    raise RuntimeError("CRAWLER_API_KEY environment variable is not set")
 app = FastAPI()
-
+app.include_router(crawler.router)
 
 @app.get("/")
 async def root() -> object:
     return {"message": "Hello World"}
-
-@app.get("/crawl")
-async def dummyCrawl(token: Optional[str] = Header(None)) -> object:
-    if CRAWLER_API_KEY != token:
-        raise HTTPException(status_code=403, detail="Forbidden")
-
-    global call_count
-    with lock:
-        call_count += 1
-        current_count = call_count
-    return {
-        "last_deployment": start_time.isoformat() + "Z",
-        "calls": current_count
-    }
