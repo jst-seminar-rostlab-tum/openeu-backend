343bf0b698700775cb05e03746cf56832133c2f2
Magnus Singer <work@magnus-singer.com>
2025-05-12 13:42:33 +0200

    update github workflow

--- app/data_sources/scrapers/meeting_calendar_scraper.py
+++ app/data_sources/scrapers/meeting_calendar_scraper.py
@@ -1,10 +1,11 @@
-from app.core.supabase_client import supabase
-from typing import Optional
-
-from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+from typing import Optional
 from urllib.parse import quote
+
 import requests
+from bs4 import BeautifulSoup
+
+from app.core.supabase_client import supabase
 
 BASE_URL_TEMPLATE = (
     "https://www.europarl.europa.eu/plenary/en/meetings-search.html?isSubmitted=true&dateFrom={date}"
@@ -13,13 +14,13 @@ BASE_URL_TEMPLATE = (
 
 
 def scrape_meeting_calendar(start_date: str, end_date: str) -> None:
-    start_date = datetime.strptime(start_date, '%d-%m-%Y')
-    end_date = datetime.strptime(end_date, '%d-%m-%Y')
+    start_date_dated = datetime.strptime(start_date, "%d-%m-%Y")
+    end_date_dated = datetime.strptime(end_date, "%d-%m-%Y")
 
-    current_date = start_date
+    current_date = start_date_dated
 
-    while current_date <= end_date:
-        date_str = quote(current_date.strftime('%d/%m/%Y'))
+    while current_date <= end_date_dated:
+        date_str = quote(current_date.strftime("%d/%m/%Y"))
         page_number = 0
         while True:
             full_url = BASE_URL_TEMPLATE.format(date=date_str, page=page_number)
@@ -38,8 +39,8 @@ def fetch_and_process_page(full_url: str) -> Optional[BeautifulSoup]:
         return None
     if response.status_code != 200:
         return None
-    soup = BeautifulSoup(response.text, 'html.parser')
-    error_message = soup.find('div', class_='message_error')
+    soup = BeautifulSoup(response.text, "html.parser")
+    error_message = soup.find("div", class_="message_error")
     if error_message and "No result" in error_message.get_text(strip=True):
         return None
     extract_meeting_info(soup)
@@ -47,32 +48,32 @@ def fetch_and_process_page(full_url: str) -> Optional[BeautifulSoup]:
 
 
 def extract_meeting_info(soup: BeautifulSoup) -> None:
-    meetings_container = soup.find('div', class_='listcontent')
-    meetings = meetings_container.find_all('div', class_='notice')
+    meetings_container = soup.find("div", class_="listcontent")
+    meetings = meetings_container.find_all("div", class_="notice")
 
     batch = []
 
     for meeting in meetings:
-        title_tag = meeting.find('p', class_='title')
+        title_tag = meeting.find("p", class_="title")
 
         title = title_tag.getText(strip=True) if title_tag else None
 
-        session_tag = meeting.find('div', class_='session')
+        session_tag = meeting.find("div", class_="session")
 
         if not session_tag:
             continue
 
-        subtitles_tag = session_tag.find('div', class_='subtitles')
+        subtitles_tag = session_tag.find("div", class_="subtitles")
         subtitles = subtitles_tag.get_text(strip=True) if subtitles_tag else None
 
-        info_tag = session_tag.find('div', class_='info')
+        info_tag = session_tag.find("div", class_="info")
 
         if not info_tag:
             continue
 
-        date_tag = info_tag.find('p', class_='date')
-        hour_tag = info_tag.find('p', class_='hour')
-        place_tag = info_tag.find('p', class_='place')
+        date_tag = info_tag.find("p", class_="date")
+        hour_tag = info_tag.find("p", class_="hour")
+        place_tag = info_tag.find("p", class_="place")
 
         if not (date_tag and hour_tag and place_tag):
             continue
@@ -86,12 +87,7 @@ def extract_meeting_info(soup: BeautifulSoup) -> None:
         except ValueError:
             continue
 
-        batch.append({
-            "title": title,
-            "datetime": datetime_obj.isoformat(),
-            "place": place,
-            "subtitles": subtitles
-        })
+        batch.append({"title": title, "datetime": datetime_obj.isoformat(), "place": place, "subtitles": subtitles})
 
     if batch:
         supabase.table("ep_meetings").insert(batch).execute()
