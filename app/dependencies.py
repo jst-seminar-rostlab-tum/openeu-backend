312e83fa8056cc2b73a7566d3f83831461d9a891
Bohdan Garchu <bohdan.garchu@gmail.com>
2025-05-16 09:57:48 +0200

    minor: fail if crawler key not set

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,12 @@
-import os
 from typing import Optional
 
-from dotenv import find_dotenv, load_dotenv
 from fastapi import Header, HTTPException
 
 from app.core.config import Settings
 
 settings = Settings()
+if settings.get_crawler_api_key() == "":
+    raise ValueError("CRAWLER_API_KEY environment variable is not set.")
 
 async def get_token_header(token: Optional[str] = Header(None)):
     if token != settings.get_crawler_api_key():
