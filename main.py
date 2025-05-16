e0aecc626cc2a13c489abe3cade56d6bd51439cc
Nils Jansen <mail@nils-jansen.com>
2025-05-16 13:14:09 +0200

    feat: schedules dummy jobs

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from app.api.crawler import router as api_crawler
 from app.api.meetings import router as api_meetings
 from app.api.scheduler import router as api_scheduler
+from app.core.jobs import setup_scheduled_jobs
+
+setup_scheduled_jobs()
 
 app = FastAPI()
 app.include_router(api_meetings)
