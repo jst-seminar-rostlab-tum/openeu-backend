e0f880c264b05fa7b1494a2ed5ad30b459c213d2
Jan-Michael van der Linde <108940700+vdl010@users.noreply.github.com>
2025-05-17 18:47:14 +0200

    added sql table

--- supabase/schemas/documents_embeddings.sql
+++ supabase/schemas/documents_embeddings.sql
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 create extension vector;
+=======
+>>>>>>> 1cbd12a (added sql table)
 
 create table documents_embeddings (
   id            uuid             primary key default gen_random_uuid(),
   source_table  text             not null,
   source_id     text             not null,
+<<<<<<< HEAD
   content_column  text             not null,   -- 'protocol', 'calendar', 'document'
   content_text  text             not null,
   embedding     vector(1536)      not null,   
@@ -52,3 +56,15 @@ begin
     limit match_count;
 end;
 $$;
+=======
+  content_type  text             not null,   -- 'protocol', 'calendar', 'document'
+  content_text  text             not null,
+  embedding     vector(1536)      not null,   
+  created_at    timestamptz      default now()
+  CONSTRAINT no_duplicates UNIQUE (source_table, source_id, content_text)
+
+);
+create index on documents_embeddings using ivfflat(embedding vector_l2_ops) with (lists = 100);
+
+--TODO: Analyze should be run on the table every 10000 updates or so to keep ivfflat efficient!
+>>>>>>> 1cbd12a (added sql table)
