da3ed57530945a9755f17f82134328fa324ab717
Jan-Michael van der Linde <108940700+vdl010@users.noreply.github.com>
2025-05-18 15:14:55 +0200

    added search functionality

--- app/core/vector_search.py
+++ app/core/vector_search.py
@@ -1,16 +1,20 @@
+# app/core/vector_search.py
+
+from typing import Dict, List
+
 from openai import OpenAI
 
 from app.core.config import Settings
 from app.core.supabase_client import supabase
 
 setting = Settings()
-openai = OpenAI(api_key=setting.get_openai_api_key())
+openai = OpenAI(api_key=setting.get_supabase_api_key())
 
 EMBED_MODEL = "text-embedding-ada-002"
 EMBED_DIM = 1536
 
 
-def get_top_k_neighbors(query: str, allowed_sources: dict[str, str], k: int = 5) -> list[dict]:
+def get_top_k_neighbors(query: str, allowed_sources: Dict[str, str], k: int = 5) -> List[Dict]:
     """
     allowed_sources = {
         "table":"colom"
@@ -23,11 +27,7 @@ def get_top_k_neighbors(query: str, allowed_sources: dict[str, str], k: int = 5)
     cols = list(allowed_sources.values())
 
     resp = supabase.rpc(
-        "match_filtered", 
-        {
-            "src_tables": tables, "content_columns": cols,
-            "query_embedding": embedding, "match_count": k
-        }
+        "match_filtered", {"src_tables": tables, "content_coloms": cols, "query_embedding": embedding, "match_count": k}
     ).execute()
 
     return resp.data
