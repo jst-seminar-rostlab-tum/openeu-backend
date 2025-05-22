48a121943dd1dc54908acd94968c46939440474d
Trung Nguyen <trungnguyenb0k30@gmail.com>
2025-05-22 14:41:01 +0200

    remove uncessary var

--- app/core/supabase_client.py
+++ app/core/supabase_client.py
@@ -1,12 +1,10 @@
 import os
 
 from app.core.config import Settings
-from supabase import Client, create_client  # type: ignore[attr-defined]
+from supabase import Client, create_client  
 
 settings = Settings()
 
-
-SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_KEY: str = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-
 supabase: Client = create_client(settings.get_supabase_project_url(), settings.get_supabase_api_key())
+
+
