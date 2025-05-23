ea06471db97267f861e7a651829383c930b9842f
Magnus Singer <work@magnus-singer.com>
2025-05-23 16:28:16 +0200

    feat: add db schema

---
+++ supabase/schemas/chat_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS chat_sessions (
+    id SERIAL PRIMARY KEY,
+    user_id TEXT,
+    title TEXT
+);
+
+CREATE TABLE IF NOT EXISTS chat_messages (
+    id SERIAL PRIMARY KEY,
+    chat_session SERIAL,
+    content TEXT,
+    author TEXT,
+    thread_id TEXT,
+
+    CONSTRAINT fk_chat_sessions
+      FOREIGN KEY (chat_session)
+      REFERENCES chat_sessions(id)
+      ON DELETE CASCADE
+);
