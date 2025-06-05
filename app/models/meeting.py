9c53bc0a3b3c432cfa681889bfd4c89fc714ad01
juliuskleinle <ju-kleinle@web.de>
2025-06-05 14:13:43 +0200

    fix again

--- app/models/meeting.py
+++ app/models/meeting.py
@@ -8,6 +8,8 @@ class Meeting(BaseModel):
     meeting_id: str
     source_table: str
     title: str
+    status: Optional[str] = None
+    meeting_url: Optional[str] = None
     meeting_start_datetime: datetime
     meeting_end_datetime: Optional[datetime] = None
     location: Optional[str] = None
