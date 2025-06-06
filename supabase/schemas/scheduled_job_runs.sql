b7d8ca435f4933fe4a7d2a6430670b3be928fc38
Bohdan Garchu <bohdan.garchu@gmail.com>
2025-06-06 13:56:21 +0200

    minor: trailing comma and name

--- supabase/schemas/scheduled_job_runs.sql
+++ supabase/schemas/scheduled_job_runs.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS "scheduled_job_runs" (
+CREATE TABLE IF NOT EXISTS scheduled_job_runs (
   "id" BIGSERIAL PRIMARY KEY,
   "name" VARCHAR(255),
   "last_run_at" TIMESTAMP,
   "success" BOOLEAN,
   "inserted_rows" BIGINT,
-  "error_msg" TEXT,
+  "error_msg" TEXT
 );
