4e2d70036038fe355a4cfed2d83c5e771f89705f
Nils Jansen <mail@nils-jansen.com>
2025-06-07 21:48:38 +0200

    feat: adds tweets table

---
+++ supabase/schemas/tweets.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tweets (
+    id TEXT PRIMARY KEY,
+    text TEXT NOT NULL,
+    author JSONB NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    reply_count INTEGER DEFAULT 0,
+    view_count INTEGER DEFAULT 0,
+    like_count INTEGER DEFAULT 0,
+    quote_count INTEGER DEFAULT 0,
+    quoted_tweet JSONB NULL,
+    retweeted_tweet JSONB NULL,
+    scraped_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
