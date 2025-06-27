484a667fa41d32ce8c6eef6495e339c867ac33ad
Max-vS <max@vonstorch.com>
2025-06-27 21:40:19 +0200

    fix: merge main

--- supabase/schemas/search_legislation_suggestions.sql
+++ supabase/schemas/search_legislation_suggestions.sql
@@ -3,14 +3,14 @@ create extension if not exists pg_trgm;
 
 create or replace function search_legislation_suggestions(search_text text)
 returns table (
-  reference text,
+  id text,
   title text,
   similarity_score float
 )
 language sql
 as $$
   select DISTINCT
-    reference,
+    id,
     title,
     similarity(title, search_text) as similarity_score
   from legislative_files
