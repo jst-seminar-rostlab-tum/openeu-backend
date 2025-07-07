91c4177b7cbd9a94ff7abf9ce6c75cfd9cdb7bba
leon-schmid <leon.cs.schmid@tum.de>
2025-07-07 11:34:33 +0200

    feat: add pdf extractor

---
+++ app/core/pdf_extractor.py
@@ -0,0 +1,26 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+
+def extract_text_from_pdf(pdf_path: str) -> str:
+    """
+    Extracts all text from a PDF file using PyMuPDF.
+
+    Args:
+        pdf_path (str): Path to the PDF file.
+
+    Returns:
+        str: The extracted text from the PDF.
+    Raises:
+        Exception: If extraction fails.
+    """
+    try:
+        loader = PyPDFLoader(pdf_path)
+        docs = loader.load()
+        return docs
+    except Exception as e:
+        raise Exception(f"Failed to extract text from PDF '{pdf_path}': {e}") from e
+
+
+if __name__ == "__main__":
+    with open("app/core/DEV.txt", "w") as f:
+        f.write(str(extract_text_from_pdf("app/core/TEST_PDF.pdf")))
