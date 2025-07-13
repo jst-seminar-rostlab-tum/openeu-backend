c71d82c74b7785aab558c5b02744c5f278dda9b9
leon-schmid <leon.cs.schmid@tum.de>
2025-07-13 18:05:56 +0200

    feat: update migration

--- supabase/migrations/20250713160518_add_legislative_procedure_files_table.sql
+++ supabase/migrations/20250713160518_add_legislative_procedure_files_table.sql
@@ -1,3 +1,5 @@
+drop function if exists "public"."match_filtered"(query_embedding vector, match_count integer, src_tables text[], content_columns text[]);
+
 create table "public"."legislative_procedure_files" (
     "id" text not null,
     "link" text not null,
@@ -9,6 +11,38 @@ create table "public"."legislative_procedure_files" (
 CREATE UNIQUE INDEX legislative_procedure_files_pkey ON public.legislative_procedure_files USING btree (id);
 
 alter table "public"."legislative_procedure_files" add constraint "legislative_procedure_files_pkey" PRIMARY KEY using index "legislative_procedure_files_pkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.match_filtered(query_embedding vector, match_count integer, src_tables text[] DEFAULT NULL::text[], content_columns text[] DEFAULT NULL::text[], source_id_param text DEFAULT NULL::text)
+ RETURNS TABLE(source_table text, source_id text, content_text text, similarity double precision)
+ LANGUAGE plpgsql
+AS $function$
+declare
+  max_dist float := sqrt(1536);  -- approx. 39.1918
+begin
+  return query
+    select
+      e.source_table,
+      e.source_id,
+      e.content_text,
+      ((1 - (e.embedding <#> query_embedding))/2) as similarity
+    from documents_embeddings e
+    where
+      (
+        src_tables is null
+        or e.source_table = any(src_tables))
+      and (
+        content_columns is null
+        or e.content_column = any(content_columns))
+      and (
+        source_id_param is null
+        or e.source_id = source_id_param)
+    order by e.embedding <#> query_embedding
+    limit match_count;
+end;
+$function$
+;
 
 grant delete on table "public"."legislative_procedure_files" to "anon";
 
