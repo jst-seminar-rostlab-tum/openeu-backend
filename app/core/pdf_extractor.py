670e75b5a5b636202c95736af7796e84cc41ff28
leon-schmid <leon.cs.schmid@tum.de>
2025-07-15 10:34:40 +0200

    fix: remove local testing

--- app/core/pdf_extractor.py
+++ app/core/pdf_extractor.py
@@ -19,8 +19,3 @@ def extract_text_from_pdf(pdf_path: str) -> str:
         return "\n".join([doc.page_content for doc in docs])    
     except Exception as e:
         raise Exception(f"Failed to extract text from PDF '{pdf_path}': {e}") from e
-
-
-if __name__ == "__main__":
-    with open("app/core/DEV.txt", "w") as f:
-        f.write(extract_text_from_pdf("app/core/TEST_PDF.pdf"))
